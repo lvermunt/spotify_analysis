7fa567a0d1e80061f8e17deddb78c3dcff70a0c8
lvermunt <luuk.vermunt@cern.ch>
2023-12-21 20:41:10 +0100

    Fix pytest no-file-found issue part3

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -6,22 +6,25 @@ import json
 from spotify_analysis.spotify import Spotify
 from spotify_analysis.utils import get_spotify_history, modify_columns_spotify_history
 
-df_songs = get_spotify_history('../tests/')
+df_songs = get_spotify_history('./')
 df_songs = modify_columns_spotify_history(df_songs)
 
 spotify_api = Spotify(reference='tracks')
 
 # TODO: Test API (via both requests and aiohttp with github secrets)
 
-with open('../tests/Spotify_Response_Sample_Track.json', 'r', encoding='utf-8') as f:
+with open('./Spotify_Response_Sample_Track.json', 'r', encoding='utf-8') as f:
     output = json.load(f)
     list_uri = ['2up3OPMp9Tb4dAKM2erWXQ']
     metadata = ['name', 'duration_ms', 'explicit', 'popularity']
     spotify_api.fill_metadata_dictionary(output, list_uri, metadata)
 
-with open('../tests/Spotify_Response_Sample_Tracks.json', 'r', encoding='utf-8') as f:
+with open('./Spotify_Response_Sample_Tracks.json', 'r', encoding='utf-8') as f:
     output = json.load(f)
     list_uri = ['2up3OPMp9Tb4dAKM2erWXQ_v1', '2up3OPMp9Tb4dAKM2erWXQ_v2']
     metadata = ['name', 'duration_ms', 'explicit', 'popularity']
     spotify_api2 = Spotify(reference='tracks')
+    spotify_api2.fill_metadata_dictionary(output, list_uri, metadata)
+
+    metadata = [['artists', 'id']]
     spotify_api2.fill_metadata_dictionary(output, list_uri, metadata)
