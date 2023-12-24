3cb8cbf072e8094e03ca55389751cc562dfcbe01
lvermunt <luuk.vermunt@cern.ch>
2023-12-24 01:31:50 +0100

    Fix spelling errors part 2

--- spotify_analysis/utils.py
+++ spotify_analysis/utils.py
@@ -7,19 +7,19 @@ import pandas as pd
 
 def get_spotify_history(inputdir):
     """
-    Loads the .json files containing your spotify streaming history and
+    Loads the .json files containing your Spotify streaming history and
     converts them to a single pandas dataframe
 
     Parameters
     ----------
     inputdir : str
-        The path to the input directory where your spotify streaming
+        The path to the input directory where your Spotify streaming
         history json files are stored
 
     Return
     ------
     pandas.Series
-        Full spotify streaming history
+        Full Spotify streaming history
     """
 
     # Find the input files
@@ -41,20 +41,20 @@ def get_spotify_history(inputdir):
 
 def modify_columns_spotify_history(df):
     """
-    Modifies some of the default names of the spotify streaming history
+    Modifies some of the default names of the Spotify streaming history
     Includes a unique track ID column (consisting out of artist and track)
     Stores the single uri in a separate column
 
     Parameters
     ----------
     df : pandas.Series
-        Pandas dataframe containing the full spotify streaming history as
+        Pandas dataframe containing the full Spotify streaming history as
         loaded by `get_spotify_history(inputdir)`
 
     Return
     ------
     pandas.Series
-        Updated Pandas dataframe with spotify streaming history
+        Updated Pandas dataframe with Spotify streaming history
     """
 
     # Remove some prefixes for easier naming
